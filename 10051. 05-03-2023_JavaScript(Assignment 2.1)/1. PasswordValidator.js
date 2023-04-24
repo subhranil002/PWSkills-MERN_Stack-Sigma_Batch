75edde6efd5a57cb15fa58f568e9d76b239b7c19
Subhranil Chakraborty <106914208+subhranil002@users.noreply.github.com>
2023-04-25 01:31:08 +0530

    Delete 10051. 05-03-2023_JavaScript(Assignment 2.1) directory

--- 10051. 05-03-2023_JavaScript(Assignment 2.1)/1. PasswordValidator.js
+++
@@ -1,4 +0,0 @@
-let password = "hey@123";
-let cpassword = "hey@124";
-
-password === cpassword ? console.log("Password Matched. Password validation Successful.") : console.log("Password didn't match. Password validation unsuccessful");
--- 10051. 05-03-2023_JavaScript(Assignment 2.1)/5. Capitalize.js
+++
@@ -1,9 +0,0 @@
-function toUpperCase(names) {
-    return (names.charCodeAt(0) >= 97)
-    ? names = String.fromCharCode(names.charCodeAt(0)-32) + names.slice(1)
-    : names;
-}
-
-let names = "subhranil";
-names = toUpperCase(names);
-console.log(names);

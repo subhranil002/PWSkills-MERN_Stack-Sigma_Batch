c94b50b3aaca4ee742f34f953770abee412d24a7
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2023-10-02 22:41:28 +0530

    updated

--- 10201. 23-08-2023_Layout_&_Spacing(TailwindCSS)/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./dist/*.html"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
--- 10206. 28-08-2023_Advanced_Project(TailwindCSS)/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./dist/index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-

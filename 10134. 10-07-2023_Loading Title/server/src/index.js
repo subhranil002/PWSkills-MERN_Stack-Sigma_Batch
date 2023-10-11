cdceb7e182f6491a6c54922a8d2c8f3bea8aefc4
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2023-10-11 22:47:51 +0530

    updated

--- 10134. 10-07-2023_Loading Title/server/src/index.js
+++
@@ -1,9 +0,0 @@
-import app from "./app.js";
-import connectionToDb from "../config/dbConnection.js";
-connectionToDb();
-
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});

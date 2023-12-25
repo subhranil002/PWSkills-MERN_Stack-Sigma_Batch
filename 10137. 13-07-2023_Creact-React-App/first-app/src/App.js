abfa4f7622af158a379d25d07f8f7e4d49a22f49
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2023-12-25 23:12:33 +0530

    updated

---
+++ 10137. 13-07-2023_Creact-React-App/first-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
---
+++ 10137. 13-07-2023_Creact-React-App/vite-react/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      hello
+    </>
+  )
+}
+
+export default App

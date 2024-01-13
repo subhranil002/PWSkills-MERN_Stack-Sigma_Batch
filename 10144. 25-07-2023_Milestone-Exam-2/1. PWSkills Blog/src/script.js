5cbf9db35d061bf444e2e17d1c92625d3c90dfa8
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-01-14 02:14:07 +0530

    updated

--- 10144. 25-07-2023_Milestone-Exam-2/1. PWSkills Blog/src/script.js
+++ 10144. 25-07-2023_Milestone-Exam-2/1. PWSkills Blog/src/script.js
@@ -1,43 +1,17 @@
-// Control Popup On/Off
-const togglePopup = () => {
-    const popup = document.getElementById("popup");
-    const overlay = document.getElementById("overlay");
-
-    if (popup.style.display === "none") {
-        popup.style.display = "flex";
-        overlay.style.display = "block";
-        document.body.style.overflow = "hidden";
-    } else {
-        popup.style.display = "none";
-        overlay.style.display = "none";
-        document.body.style.overflow = "auto";
-    }
-};
-
-// Get Read Button Clicked Blog
-document.querySelector("#blogs").addEventListener("click", function (e) {
-    if (e.target.id === "readMore") {
-        console.log(e.target.parentElement);
-    }
-});
-
-// Sample data for the blog post
-const blogData = {
-    imageUrl: "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
-    title: "Lorem ipsum dolor sit amet, consectetur",
-    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit"
-};
-
-// Function to generate the blog post HTML
+// Function to generate the blog Post
 function generateBlogPost(data) {
     // Create the main container
     const blogContainer = document.createElement("div");
-    blogContainer.className = "w-[250px] shadow-[0_1px_3px_2px_rgba(0,0,0,0.3)] rounded-lg flex flex-col justify-between items-center py-5 gap-3";
+    blogContainer.className =
+        "w-[250px] shadow-[0_1px_3px_2px_rgba(0,0,0,0.3)] rounded-lg flex flex-col justify-between items-center py-5 gap-3 overflow-hidden";
+    blogContainer.id = data.id;
 
     // Create the image element
     const image = document.createElement("img");
     image.className = "rounded-lg w-[200px] h-[200px] object-cover";
-    image.src = data.imageUrl;
+    image.src =
+        data.imageUrl ||
+        "https://iconicentertainment.in/wp-content/uploads/2013/11/dummy-image-square.jpg";
     image.alt = "image";
 
     // Create the content container
@@ -47,12 +21,20 @@ function generateBlogPost(data) {
     // Create the title element
     const title = document.createElement("h1");
     title.className = "font-semibold text-base";
-    title.textContent = data.title;
+    const truncatedTitle = data.title.slice(0, 45);
+    const finalTitle =
+        data.title.length > 53 ? truncatedTitle + "..." : truncatedTitle;
+    title.textContent = finalTitle;
 
     // Create the description element
     const description = document.createElement("p");
     description.className = "text-sm text-gray-900";
-    description.textContent = data.description;
+    const truncatedDescription = data.description.slice(0, 55);
+    const finalDescription =
+        data.description.length > 58
+            ? truncatedDescription + "..."
+            : truncatedDescription;
+    description.textContent = finalDescription;
 
     // Create the "Read More" button
     const readMoreBtn = document.createElement("button");
@@ -69,8 +51,73 @@ function generateBlogPost(data) {
     blogContainer.appendChild(readMoreBtn);
 
     const blogsContainer = document.getElementById("blogs");
-    blogsContainer.appendChild(blogContainer)
+    blogsContainer.appendChild(blogContainer);
 }
 
-generateBlogPost(blogData);
+// Retrive Data from Local Storage
+let blogArray = [];
+function getBlogDataFromLocalStorage() {
+    const storedBlogArray = localStorage.getItem("blogDataArray");
+    blogArray = JSON.parse(storedBlogArray);
 
+    if (blogArray) {
+        blogArray.forEach((blogData) => {
+            generateBlogPost(blogData);
+        });
+    }
+}
+getBlogDataFromLocalStorage();
+
+// Control Popup On/Off
+const togglePopup = () => {
+    const popup = document.getElementById("popup");
+    const overlay = document.getElementById("overlay");
+
+    if (popup.style.display === "none") {
+        popup.style.display = "flex";
+        overlay.style.display = "block";
+        document.body.style.overflow = "hidden";
+    } else {
+        popup.style.display = "none";
+        overlay.style.display = "none";
+        document.body.style.overflow = "auto";
+    }
+};
+
+// Set New Blog Data To Local Storage
+function createNewBlog() {
+    const id = Math.random();
+    const imageUrl = document.getElementById("imageUrl").value;
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("description").value;
+    const readContent = document.getElementById("readContent").value;
+
+    if (!title || !description || !readContent) {
+        return false;
+    }
+
+    const blogData = {
+        id,
+        imageUrl,
+        title,
+        description,
+        readContent,
+    };
+
+    if (blogArray) {
+        blogArray = [...blogArray, blogData];
+    } else {
+        blogArray = [blogData];
+    }
+
+    localStorage.setItem("blogDataArray", JSON.stringify(blogArray));
+    togglePopup();
+    location.reload();
+}
+
+// Get Read Button Clicked Blog
+document.querySelector("#blogs").addEventListener("click", function (e) {
+    if (e.target.id === "readMore") {
+        console.log(e.target.parentElement);
+    }
+});

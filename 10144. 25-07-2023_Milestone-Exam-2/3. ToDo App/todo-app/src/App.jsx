9c777f6024758bc2a16f4d01ee785c775b30c632
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-01-23 02:51:09 +0530

    updated

--- 10144. 25-07-2023_Milestone-Exam-2/3. ToDo App/todo-app/src/App.jsx
+++ 10144. 25-07-2023_Milestone-Exam-2/3. ToDo App/todo-app/src/App.jsx
@@ -1,15 +1,57 @@
 import { useState } from "react";
-import "./App.css";
-import AddToDo from "./AddToDo.jsx";
-import ToDOContainer from "./ToDoContainer.jsx";
+import ToDoCard from "./ToDoCard.jsx";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [todos, setToDo] = useState({
+        allToDos: [],
+    });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    function addToDo(event) {
+        const id = Math.random();
+        const message = event.target.elements.message.value
+        console.log(message);
+
+        setToDo((prevData) => ({
+            allToDos: [...prevData.allToDos, { id: id, message: message }],
+        }));
+        event.preventDefault();
+    }
 
     return (
-        <main className="bg-[#4f1862] w-[100vw] h-[100vh] flex flex-col items-center gap-7">
-            <AddToDo />
-            <ToDOContainer />
+        <main className="bg-[#4f1862] w-[100vw] min-h-screen pb-10 flex flex-col items-center gap-7">
+            <form
+                onSubmit={addToDo}
+                className="flex items-center justify-center gap-0 w-[100%] h-[20vh]"
+            >
+                <input
+                    className="w-[400px] py-3 px-5 rounded-l-lg h-[100px] text-2xl placeholder:text-xl outline-none"
+                    type="text"
+                    placeholder="Write Here ..."
+                    name="message"
+                    required
+                />
+                <div className="h-[100px] bg-white flex justify-center items-center pr-5 rounded-r-lg">
+                    <button
+                        type="submit"
+                        className="text-white bg-[#4f1862] py-2 w-[100px] rounded-lg"
+                    >
+                        Add
+                    </button>
+                </div>
+            </form>
+            <div className="w-[90%] flex flex-wrap justify-center gap-7">
+                {todos.allToDos.map((todo, index) => (
+                    <ToDoCard
+                        key={todo.id}
+                        index={index + 1}
+                        message={todo.message}
+                    />
+                ))}
+            </div>
         </main>
     );
 }
--- 10144. 25-07-2023_Milestone-Exam-2/3. ToDo App/todo-app/src/ToDoCard.jsx
+++ 10144. 25-07-2023_Milestone-Exam-2/3. ToDo App/todo-app/src/ToDoCard.jsx
@@ -1,27 +1,25 @@
 import { useState } from "react";
 
-function ToDoCard() {
-    const [status, setStatus] = useState(true);
+function ToDoCard({index, message}) {
+    const [status, setStatus] = useState("Pending");
 
     function updateStatus() {
-        
+        if (status === "Pending") {
+            setStatus("Completed");
+        } else {
+            setStatus("Pending");
+        }
     }
 
     return (
-        <div className="bg-[#24273d] w-[20%] text-white">
+        <div className="bg-[#24273d] text-white p-6 w-[250px] rounded-xl flex flex-col gap-3">
             <div>
-                <h1>Go to gym</h1>
+                <h1 className="w-full h-[50px] font-bold text-lg">{index}. {message}</h1>
             </div>
-            <h3>
-                Status: <span>Pending</span>
-            </h3>
-            <div>
-                <button>Update Status</button>
-                <div>
-                    <button><i class="fa-solid fa-check"></i></button>
-                    <button><i class="fa-solid fa-xmark"></i></button>
-                </div>
-                <button>Remove</button>
+            <h3>Status: {status}</h3>
+            <div className="flex flex-col justify-center items-center w-full gap-2">
+                <button className="bg-[#3425ac] w-full py-2 rounded-lg " onClick={updateStatus}>Update Status</button>
+                <button className="bg-[#3425ac] w-full py-2 rounded-lg ">Remove</button>
             </div>
         </div>
     );
